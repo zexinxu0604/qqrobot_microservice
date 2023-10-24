101589dd300df8cb8bdd1d01cbe426c3763e61ac
“xzx” <1113654557@qq.com>
2023-10-24 15:38:24 +0800

    二次封装了resttemplate的http请求

--- message_service_1/src/main/java/org/xzx/service/Gocq_service.java
+++ message_service_1/src/main/java/org/xzx/service/Gocq_service.java
@@ -12,22 +12,22 @@ import org.xzx.bean.messageBean.Send_Message.PrivateMessage;
 public class Gocq_service {
 
     @Autowired
-    private RestTemplate restTemplate;
+    private SpringRestService springRestService;
 
     @Value("${gocq.basicUrl}")
     private String basicUrl;
 
     public void send_group_message(int group_id, String message){
         SendGroupMessage groupMessage = new SendGroupMessage(group_id, message);
-        System.out.println(restTemplate.postForObject(basicUrl + "send_group_msg", groupMessage, String.class));
+        System.out.println(springRestService.postWithObject(basicUrl + "send_group_msg", groupMessage, String.class));
     }
 
     public void send_private_message(int user_id, String message) {
         PrivateMessage privateMessage = new PrivateMessage(user_id, message);
-        System.out.println(restTemplate.postForObject(basicUrl +"send_private_msg", privateMessage, String.class));
+        System.out.println(springRestService.postWithObject(basicUrl +"send_private_msg", privateMessage, String.class));
     }
 
     public JsonNode get_message(int message_id){
-        return restTemplate.getForObject(basicUrl + "get_msg?message_id=" + message_id, JsonNode.class);
+        return springRestService.getForObject(basicUrl + "get_msg?message_id=" + message_id, JsonNode.class);
     }
 }
